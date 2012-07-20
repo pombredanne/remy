eaf4837b0e29a4d2a185ef1dab197eb9fc2841a0
Greg Woodward & Noah Thorp <pair+greg+noah@pivotallabs.com>
2012-07-20 11:08:40 -0700

    Better description of spec.

--- spec/tasks/remy_spec.rb
+++ spec/tasks/remy_spec.rb
@@ -7,7 +7,7 @@ describe 'background.rake' do
     Rake::Task.define_task(:environment)
   end
 
-  it 'should run task foo' do
+  it 'should run the task to save the JSON file from the Chef yml config files' do
     Remy::Config::Chef.should_receive(:save_node_json).with('my/path/to/node.json')
     Rake.application['remy:save_node_json'].invoke('my/path/to/node.json')
   end
